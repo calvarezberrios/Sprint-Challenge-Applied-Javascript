a17a41e38bb7653dffd58fc664ced5d7b44ae9ce
Carlos E. Alvarez-Berrios <calvarezberrios@Carloss-MacBook-Pro.local>
2020-03-21 13:58:39 -0400

    Created Carousel, working on functionality

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,62 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function createCarousel() {
+  const carousel = document.createElement("div");
+  const leftButton = document.createElement("div");
+  const mountainsImg = document.createElement("img");
+  const computerImg = document.createElement("img");
+  const treesImg = document.createElement("img");
+  const turntableImg = document.createElement("img");
+  const rightButton = document.createElement("div");
+
+  carousel.append(leftButton, mountainsImg, computerImg, treesImg, turntableImg, rightButton);
+
+  carousel.classList.add("carousel");
+  leftButton.classList.add("left-button");
+  rightButton.classList.add("right-button");
+
+  leftButton.textContent = "<";
+  mountainsImg.src = "../../assets/carousel/mountains.jpeg";
+  computerImg.src = "../../assets/carousel/computer.jpeg";
+  treesImg.src = "../../assets/carousel/trees.jpeg";
+  turntableImg.src = "../../assets/carousel/turntable.jpeg";
+  rightButton.textContent = ">";
+
+  const images = Array.prototype.slice.call(carousel.querySelectorAll(".carousel img"));
+  let index = 0;
+  //let imgRight = 1;
+  images[index].style.display = "block";
+  images[++index].style.display = "block";
+
+  rightButton.addEventListener("click", (event) => {
+    images.forEach(img => img.style.display = "none");
+    
+    if (index >= images.length - 1) {
+      index = 0;
+    }
+    images[index++].style.display = "block";
+    images[index].style.display = "block";
+
+    
+
+  });
+
+  leftButton.addEventListener("click", (event) => {
+    images.forEach(img => img.style.display = "none");
+
+    if(currIndex > 0) {
+      currIndex -= 1;
+    }
+
+    images[currIndex].style.display = "block";
+    images[currIndex + 1].style.display = "block";
+  });
+
+
+  return carousel;
+}
+
+const carouselContainer = document.querySelector(".carousel-container");
+carouselContainer.appendChild(createCarousel());
